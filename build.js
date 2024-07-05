0a6c2b9125a797c983c0cc9d13b02f1da6d9c911
poteto <naonyan1135@gmail.com>
2024-07-06 00:02:51 +0900

    js一枚にcssもまとめたもの

--- build.js
+++ build.js
@@ -1,9 +1,14 @@
 const esbuild = require('esbuild');
+const inlineCss = require('esbuild-plugin-inline-css');
 
 esbuild.build({
   entryPoints: ['src/index.js'],
   bundle: true,
   minify: true,
   outfile: 'dist/bundle.js',
-  loader: { '.css': 'css' }
+  plugins: [inlineCss()],
+  loader: { '.css': 'css' },
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
 }).catch(() => process.exit(1));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import hljs from 'highlight.js/lib/common';
 import 'highlight.js/styles/atom-one-dark.min.css';
-import '../raku.css';
+import rakuStyles from '../raku.css';
+
+// CSSをインラインで埋め込む
+const style = document.createElement('style');
+style.textContent = `
+  ${require('highlight.js/styles/atom-one-dark.css')}
+  ${rakuStyles}
+`;
+document.head.append(style);
 
 document.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('pre code').forEach((block) => {
