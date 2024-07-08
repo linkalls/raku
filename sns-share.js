1e80a1dc12eaf26040d719497b8a15a33c748e4a
poteto <naonyan1135@gmail.com>
2024-07-09 00:08:55 +0900

    sns share

--- sns-share.js
+++ sns-share.js
@@ -2,14 +2,62 @@ class XShare extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
-    // Shadow DOM内に外部CSSファイルをインポート
+    const url = encodeURIComponent(document.location.href);
+    const title = encodeURIComponent(document.title);
+    const twitterShareUrl = `https://twitter.com/intent/tweet?text=${title}%0A${url}`;
+
     shadow.innerHTML = `
-    <link rel="stylesheet" href="raku.css">    
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <div class="social-icons">
-                <a href="https://twitter.com/" target="_blank"><span class="fab fa-twitter" aria-hidden="true"></span></a>
-</div>
-  `;
+      <link rel="stylesheet" href="raku.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+      <div class="social-icons">
+        <a href="${twitterShareUrl}" target="_blank" aria-label="Share on Twitter">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </div>
+    `;
+  }
+}
+
+class NormalShare extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const url = encodeURIComponent(document.location.href);
+    const title = encodeURIComponent(document.title);
+
+    // シャドウDOMの内部HTMLを設定
+    shadow.innerHTML = `
+      <link rel="stylesheet" href="raku.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+      <style>button {
+      background: none;
+  color: inherit;
+  border: none;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  margin: 0;
+  margin-top: -15px; 
+  margin-left: -10px; 
+      }</style>
+      <div class="social-icons">
+        <button aria-label="Share">
+          <i class="fas fa-share-alt"></i>
+        </button>
+      </div>
+    `;
+
+    // クリックイベントリスナーを追加
+    this.addEventListener('click', () => {
+      if (navigator.share) {
+        navigator.share({
+          title: document.title, // 元のタイトルを使用
+          text: `${document.title}\n${document.location.href}`, // タイトル、改行、URLの形式でテキストを設定
+          url: document.location.href // 元のURLを使用
+        }).then(() => console.log('Successful share'))
+          .catch((error) => console.log('Error sharing', error));
+      }
+    });
   }
 }
 
@@ -17,31 +65,24 @@ class FShare extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({ mode: "open" });
+    const url = encodeURIComponent(document.location.href);
+    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+
     shadow.innerHTML = `
-      <link rel="stylesheet" href="raku.css">    
+      <link rel="stylesheet" href="raku.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
       <div class="social-icons">
-        <a href="https://www.facebook.com/" target="_blank"><span class="fab fa-facebook" aria-hidden="true"></span></a>
+        <a href="${facebookShareUrl}" target="_blank" aria-label="Share on Facebook">
+          <i class="fab fa-facebook"></i>
+        </a>
       </div>
     `;
   }
 }
 
-class IShare extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = `
-      <link rel="stylesheet" href="raku.css">    
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-      <div class="social-icons">
-        <a href="https://www.instagram.com/" target="_blank"><span class="fab fa-instagram" aria-hidden="true"></span></a>
-      </div>
-    `;
-  }
-}
 
+
+
+customElements.define("normal-share", NormalShare);
 customElements.define("x-share", XShare);
-customElements.define("f-share", FShare);
-customElements.define("i-share", IShare);
-
+customElements.define("f-share", FShare); 
